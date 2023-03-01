e4ac49160289d8a52124790dab07a104cccfa547
Benjamin Klemencic <benjamin.klemencic@fau.de>
2023-03-01 19:48:16 +0100

    1.6.15 (Translator with loop instead of RecursiveIteratorIterator etc -> not flexible if API structures change again but quicker to roll out now)

--- includes/Translator.php
+++ includes/Translator.php
@@ -6,7 +6,8 @@ defined('ABSPATH') || exit;
 
 use function RRZE\Lectures\Config\getSanitizerMap;
 
-class Translator extends \RecursiveIteratorIterator
+// class Translator extends \RecursiveIteratorIterator
+class Translator
 {
     protected $display_language;
     protected $display_language_fallback;
@@ -28,30 +29,33 @@ class Translator extends \RecursiveIteratorIterator
             $this->display_language_fallback = $aLang[1];
         }
 
+        // set $this->all_language_codes to 2-letters only (example: ['de', 'en', 'fr'])
         $this->all_language_codes = array_map(function ($val) {
             return substr($val, 0, 2);
         }, \ResourceBundle::getLocales(''));
 
     }
 
-    public function current()
-    {
-        $current = parent::current();
-        switch($this->key()) {
-            case 'de':
-                $current = strtolower($current) . ' JUHU';
-                break;
-            default:
-                break;
-        }
-        return $current;
-    }
+    // public function current()
+    // {
+    //     $current = parent::current();
+    //     switch($this->key()) {
+    //         case 'de':
+    //             $current = strtolower($current) . ' JUHU';
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    //     return $current;
+    // }
 
 
+    
+    /* returns translations by language (given attribute and/or settings value) or '' */
     private function getTranslation(&$aIn)
     {
         if (!is_array($aIn)) {
-            // DIP-Field is string (and not array with language codes) = DIP-field is not a multilang field (["en" => "english text", "de" => "deutscher Text"])
+            // DIP-Field is not a mulitlang-field (== string (and not array with language codes) (["en" => "english text", "de" => "deutscher Text"])
             return $aIn;
         }
 
@@ -65,19 +69,19 @@ class Translator extends \RecursiveIteratorIterator
     }
 
 
-    private function maybeSetTranslation(&$item, $key)
-    {
-        $this->aPath .= $key . '/';
-        // if (in_array($key, $this->all_language_codes)) {
-        //     $item = 'PHP Rocks'; // Do This!
-        //     var_dump($this->aPath);
-        //     exit;
-        //     // prev($this->mirrorData); 
+    // private function maybeSetTranslation(&$item, $key)
+    // {
+    //     $this->aPath .= $key . '/';
+    //     // if (in_array($key, $this->all_language_codes)) {
+    //     //     $item = 'PHP Rocks'; // Do This!
+    //     //     var_dump($this->aPath);
+    //     //     exit;
+    //     //     // prev($this->mirrorData); 
 
-        // }
-        var_dump($this->aPath);
+    //     // }
+    //     var_dump($this->aPath);
 
-    }
+    // }
 
 
     public function setTranslations(&$aData)
@@ -85,60 +89,158 @@ class Translator extends \RecursiveIteratorIterator
 
         // echo '<pre>';
         // var_dump($aData);
-        // $test = json_decode(json_encode($aData));
+
+        foreach ($aData as $nr => $aLecture) {
+            foreach ($aLecture['main'] as $key => $field) {
+                if (in_array($key, $this->all_language_codes)) {
+                    $aData[$nr]['main'][$field] = $this->getTranslation($aData[$nr]['main'][$field][$key]);
+                    echo '<pre>';
+                    echo 'translated! ';
+                    var_dump($aData);
+                    exit;
+                }
+            }
+        }
 
 
-        $iterator = new Translator(new \RecursiveIteratorIterator(new \RecursiveArrayIterator($aData), \RecursiveIteratorIterator::SELF_FIRST));
-        foreach ($iterator as $k => $v) {
+            // // echo '<pre>';
+            // // var_dump($aLecture['providerValues']['courses']);
+            // // exit;
 
-            // // if (is_array($k)){
-            // //     echo 'key ist ein array';
-            // //     exit;
-            // // }
-            // // if (is_array($v)){
-            // //     echo 'value ist ein array bei key=' . $k;
-            // //     exit;
-            // // }
-            // // echo "key = " . $k . ' ';
-            // if (is_array($v)) {
-            //     // echo '<pre>';
-            //     // var_dump($v);
+            // // foreach($aLectures as $lNr => $aLecture){
 
-            //     if (!empty(array_intersect(array_keys($v), $this->all_language_codes))){
-            //         // echo 'found';
-            //         // exit;
+            // foreach ($aLecture['providerValues']['event_orgunit'] as $eNr => $aDetails) {
 
-            //         // $k = $this->getTranslation($v);
-            //         // echo '<pre>';
-            //         // var_dump($k);
-            //         // exit;
-
-            //         // $iterator->getInnerIterator()->offsetSet($k, 0);
-            //         $iterator[$k] = 'JUHU';
-
-            //         // exit;
-
+            //     foreach ($this->multlangFields['event_orgunit'] as $field) {
+            //         $aData[$nr]['providerValues']['event_orgunit'][$eNr][$field] = $this->getTranslation($aDetails[$field]);
             //     }
             // }
 
-            // echo '<hr>';
+            // // foreach($aLecture['providerValues']['event'] as $eNr => $aDetails){
+            // //     foreach($this->multlangFields['event'] as $field){
+            // //         $aData[$nr]['providerValues']['event'][$eNr][$field] = $this->getTranslation($aDetails[$field]);
+            // //     }
+            // // }
+
+            // foreach ($aLecture['providerValues']['courses'] as $cNr => $aCourses) {
+            //     // echo '<pre>';
+            //     // var_dump($aLecture['providerValues']);
+            //     // exit;
+
+
+            //     foreach ($this->multlangFields['courses'] as $field) {
+            //         // echo '<pre>' . $field . '<br>' . 
+            //         // // $this->getTranslation($aCourses[$field]) . ' +';
+            //         // var_dump($aData[$nr]['providerValues']['courses'][$cNr]);
+            //         // exit;
+            //         $aData[$nr]['providerValues']['courses'][$cNr][$field] = $this->getTranslation($aCourses[$field]);
+            //     }
+
+            //     // if (!empty($aCourses['planned_dates'])){
+            //     //     echo '<pre>';
+            //     //     var_dump($aCourses['planned_dates']);
+            //     //     exit;
+            //     // }else{
+            //     //     echo '<pre>';
+            //     //     var_dump($aCourses);
+            //     //     exit;
+
+            //     // }
+
+            //     if (!empty($aCourses['planned_dates'])) {
+            //         foreach ($aCourses['planned_dates'] as $pNr => $aDetails) {
+            //             foreach ($this->multlangFields['planned_dates'] as $field) {
+            //                 $aData[$nr]['providerValues']['courses'][$cNr]['planned_dates'][$pNr][$field] = $this->getTranslation($aDetails[$field]);
+            //             }
+            //         }
+            //     }
+            // }
+
+            // foreach ($aLecture['providerValues']['modules'] as $mNr => $aModule) {
+            //     foreach ($this->multlangFields['modules'] as $field) {
+            //         $aData[$nr]['providerValues']['modules'][$mNr][$field] = $this->getTranslation($aModule[$field]);
+            //     }
+            //     foreach ($aModule['modules_cos'] as $iNr => $aDetails) {
+            //         foreach ($this->multlangFields['modules_cos'] as $field) {
+            //             $aData[$nr]['providerValues']['modules'][$mNr]['modules_cos'][$iNr][$field] = $this->getTranslation($aDetails[$field]);
+            //         }
+            //     }
+            //     foreach ($aModule['stud'] as $iNr => $aDetails) {
+            //         foreach ($this->multlangFields['stud'] as $field) {
+            //             $aData[$nr]['providerValues']['modules'][$mNr]['stud'][$iNr][$field] = $this->getTranslation($aDetails[$field]);
+            //         }
+            //     }
+            //     foreach ($aModule['modules_restrictions'] as $iNr => $aDetails) {
+            //         foreach ($this->multlangFields['stud'] as $field) {
+            //             $aData[$nr]['providerValues']['modules'][$mNr]['modules_restrictions'][$iNr][$field] = $this->getTranslation($aDetails[$field]);
+            //         }
+            //     }
+            // }
+            // }
         }
-
-        echo '<pre>test ist ';
-        var_dump($aData);
-        // var_dump($iterator->getArrayCopy());
-        // var_dump($iterator);
+    }
 
 
-        echo 'setTranslations()<br>';
 
+
+
+
+
+        // $test = json_decode(json_encode($aData));
+        // $iterator = new Translator(new \RecursiveIteratorIterator(new \RecursiveArrayIterator($test), \RecursiveIteratorIterator::SELF_FIRST));
+
+
+        // $iterator = new Translator(new \RecursiveIteratorIterator(new \RecursiveArrayIterator($aData), \RecursiveIteratorIterator::SELF_FIRST));
+        // foreach ($iterator as $k => $v) {
+
+        //     // // if (is_array($k)){
+        //     // //     echo 'key ist ein array';
+        //     // //     exit;
+        //     // // }
+        //     // // if (is_array($v)){
+        //     // //     echo 'value ist ein array bei key=' . $k;
+        //     // //     exit;
+        //     // // }
+        //     // // echo "key = " . $k . ' ';
+        //     // if (is_array($v)) {
+        //     //     // echo '<pre>';
+        //     //     // var_dump($v);
+
+        //     //     if (!empty(array_intersect(array_keys($v), $this->all_language_codes))){
+        //     //         // echo 'found';
+        //     //         // exit;
+
+        //     //         // $k = $this->getTranslation($v);
+        //     //         // echo '<pre>';
+        //     //         // var_dump($k);
+        //     //         // exit;
+
+        //     //         // $iterator->getInnerIterator()->offsetSet($k, 0);
+        //     //         $iterator[$k] = 'JUHU';
+
+        //     //         // exit;
+
+        //     //     }
+        //     // }
+
+        //     // echo '<hr>';
+        // }
+
+        // echo '<pre>test ist ';
         // var_dump($aData);
-        exit;
+        // // var_dump($iterator->getArrayCopy());
+        // // var_dump($iterator);
 
-        // array_walk_recursive($aData, [$this, 'maybeSetTranslation']);
 
-        // var_dump($aData);
+        // echo 'setTranslations()<br>';
+
+        // // var_dump($aData);
         // exit;
+
+        // // array_walk_recursive($aData, [$this, 'maybeSetTranslation']);
+
+        // // var_dump($aData);
+        // // exit;
 
 
 
