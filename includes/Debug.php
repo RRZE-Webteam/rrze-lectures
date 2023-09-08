b06df0bd1f115f73514dab31daf36238c25834d8
Wolfgang Wiese <xwolf@xwolf.de>
2023-09-08 11:35:49 +0200

    Neues Dump für Debug. Kann auch für Console verwendet werden und ist MEM-Speicherfreundlicher als das alte var_dump() .

--- includes/Debug.php
+++ includes/Debug.php
@@ -14,9 +14,10 @@ class Debug {
    
    // Gets an Variable, that can be an string, array or object and modifies its output in a more readable form 
    public static function get_html_var_dump($input) {
-        $out = Debug::get_var_dump($input);
+//        $out = Debug::get_var_dump($input);
+         $out = self::get_dump_debug($input);
         
-        
+        /*
         $patterns_replacements = array( 
             '/=>[\r\n\s]+/'         => ' => ',            
             "/\s+bool\(true\)/"     => ' <span style="color:green">TRUE</span>,',
@@ -34,7 +35,8 @@ class Debug {
         if (!empty($processed_string)) {
             $out = $processed_string;
         }
-         return '<pre class="var_dump">'.$out.'</pre>';
+         */
+         return '<div class="var_dump">'.$out.'</div>';
 
     }
 
@@ -70,10 +72,191 @@ class Debug {
 
     
     // prrints var dump as variable
-    public static function get_var_dump($input) {
+    public static function get_var_dump($input) { 
         ob_start(); 
         var_dump($input);
         return "\n" . ob_get_clean();
+    }
+    
+    /*
+     * Better Dump.
+     */
+    public static function get_dump_debug($input, $collapse = false, $console = false, $htmlbreak = true) {
+        
+        
+        $recursive = function($data, $level=0) use (&$recursive, $collapse, $console, $htmlbreak) {
+            global $argv;
+            $output = '';
+            
+                
+            // $indent = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+            $indent = '';  
+            $isTerminal = isset($argv);
+            if ($console) {
+                $isTerminal = true;
+            }
+            if (!$isTerminal && $level == 0 && !defined("DUMP_DEBUG_SCRIPT")) {
+                define("DUMP_DEBUG_SCRIPT", true);
+
+                $output .= '<script language="Javascript">function toggleDisplay(id) {';
+                $output .=  'var state = document.getElementById("container"+id).style.display;';
+                $output .=  'document.getElementById("container"+id).style.display = state == "block" ? "none" : "block";';
+                $output .=  'document.getElementById("plus"+id).style.display = state == "block" ? "inline" : "none";';
+                $output .=  '}</script>'."\n";
+                $output .=  '<style>.dump_container { border-left: 1px dotted #000 !important; border-bottom: 1px dotted #000 !important; margin-left: 0; padding-left: 2em;}'
+                        . ' .dump_debug { display: inline; }'
+                        . ' .dump_fieldname { color:#dd8800; background-color: inherit;}'
+                        . ' .dump_fieldname_numeric { color: darkgreen; font-style: italic; }'
+                        . ' .dump_value { font-family: Courier, Monospace; font-weight: bold; white-space: pre; }'
+                        . ' .dump_empty_array { color: #black; }'
+                        . ' .dump_typeinfo { margin-left: 2em; color:#666666; font-size:0.9em; font-style:italic;} </style>'."\n";
+                
+            }
+
+            $type = !is_string($data) && is_callable($data) ? "Callable" : ucfirst(gettype($data));
+            $type_data = null;
+            $type_color = null;
+            $type_length = null;
+
+            switch ($type) {
+                case "String": 
+                    $type_color = "";
+                    $type_length = strlen($data);
+                    $type_data = "\"" . htmlentities($data) . "\""; break;
+
+                case "Double": 
+                case "Float": 
+                    $type = "Float";
+                    $type_color = "#0099c5";
+                    $type_length = strlen($data);
+                    $type_data = htmlentities($data); break;
+
+                case "Integer": 
+                    $type_color = "darkgreen";
+                    $type_length = strlen($data);
+                    $type_data = htmlentities($data); break;
+
+                case "Boolean": 
+                    if ($data) {
+                        $type_color = "green";
+                    } else {
+                        $type_color = "red";
+                    }
+                    $type_length = strlen($data);
+                    $type_data = $data ? "TRUE" : "FALSE"; break;
+                
+                    
+
+                case "NULL": 
+                    $type_length = 0; break;
+
+                case "Array": 
+                    $type_length = count($data);
+            }
+
+            if (in_array($type, array("Object", "Array"))) {
+                $notEmpty = false;
+
+                foreach($data as $key => $value) {
+                    if (!$notEmpty) {
+                        $notEmpty = true;
+
+                        if ($isTerminal) {
+                            $output .=  $type . ($type_length !== null ? "(" . $type_length . ")" : "")."\n";
+
+                        } else {
+                            $id = substr(md5(rand().":".$key.":".$level), 0, 8);
+                            $output .=  '<div class="dump_debug">';
+                            $output .=  "<a href=\"javascript:toggleDisplay('". $id ."');\" style=\"text-decoration:none\">";
+                            $output .=  "<strong style='color:#666666'>" . $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "</strong>";
+                            $output .=  "</a>";
+                            $output .=  "<span id=\"plus". $id ."\" style=\"color: red; display: " . ($collapse ? "inline" : "none") . ";\">&nbsp;&#10549;</span>";
+                            if ($collapse) {
+                                 $output .=  "<div id=\"container". $id ."\" class=\"dump_container\" style=\"display: none; \"><br>";
+                            } else {
+                                 $output .=  "<div id=\"container". $id ."\" class=\"dump_container\" style=\"display: block;\">";
+                            }
+                             $output .=  "\n";
+                           
+          
+                        }
+
+                        for ($i=0; $i <= $level; $i++) {
+                            $output .=  $isTerminal ? "|    " : "$indent";
+                        }
+
+                        if ($isTerminal) {
+                            $output .=  "\n";
+    
+                        }
+    
+                    }
+
+                    for ($i=0; $i <= $level; $i++) {
+                        $output .=  $isTerminal ? "|    " : "$indent";
+                    }
+                    
+                    if ($isTerminal) {
+                        $output .=  "[" . $key . "] => ";
+                    } else {
+                        if (is_numeric($key)) {
+                            $output .= "[<code class=\"dump_fieldname dump_fieldname_numeric\"'>" . $key . "</code>]";
+                        } else {
+                            $output .= "[\"<code class=\"dump_fieldname\"'>" . $key . "</code>\"]";
+                        }
+                        $output .= "&nbsp;=>&nbsp;";
+                    }
+
+                    $output .= call_user_func($recursive, $value, $level+1);
+                }
+
+                if ($notEmpty) {
+                    for ($i=0; $i <= $level; $i++) {
+                        $output .=  $isTerminal ? "|    " : "$indent";
+                    }
+
+                    if (!$isTerminal) {
+                        $output .=  "</div>";
+                        $output .=  "</div>";
+                    }
+
+                } else {
+                    $output .=  $isTerminal ? 
+                            $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "  " : 
+                            "<span class=\"dump_value dump_empty_array\">" . $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "</span>&nbsp;&nbsp;";
+                }
+
+            } else {
+                if ($type_data != null) {
+                    if (empty($type_color)) {
+                         $output .=  $isTerminal ? $type_data : "<span class=\"dump_value\">" . $type_data . "</span>";
+                    } else {
+                         $output .=  $isTerminal ? $type_data : "<span class=\"dump_value\" style='color:" . $type_color . "'>" . $type_data . "</span>";
+                    }
+                   
+                }
+                $output .=  $isTerminal ? 
+                        $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "  " : 
+                        " <span class=\"dump_typeinfo\">" . $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "</span>";
+
+                
+            }
+
+            if ($isTerminal) {
+                 $output .=  "\n";
+            } else {
+                 if ($htmlbreak) {
+                    $output .=  "<br />\n";   
+                 } else {
+                    $output .=  "\n";
+                 }
+            }
+            return $output;
+        };
+
+        $output = call_user_func($recursive, $input);
+        
+        return $output;
     }
     
     // prints a variable as notice string
@@ -81,7 +264,7 @@ class Debug {
         if (!isset($_GET['debug'])) {
             return;
         }
-        if (!self::isRRZEUser()) {
+        if (!self::isDevUser()) {
             return;
         }
         return '<div class="alert clearfix clear alert-info">'.$text.'</div>';
@@ -92,7 +275,7 @@ class Debug {
     public static function console_log(string $msg = '', float $tsStart = 0) {
         if (isset($_GET['debug'])) {
             
-            if (!self::isRRZEUser()) {
+            if (!self::isDevUser()) {
                 return;
             }
             
@@ -114,40 +297,8 @@ class Debug {
     }
     
 
-    
-    private static function isFAUUser() {
-         $knownhostsuser = [
-            '/\.uni\-erlangen\.de/i'
-        ];
-        $knownhostadresses = [
-            '/^131\.188\./i',
-            '/^10\./i'
-        ];
 
-        $found = false;	  
-        $remip = $_SERVER['REMOTE_ADDR'];
-
-        foreach ($knownhostadresses as $regexp) {
-            if (preg_match($regexp,$remip)) {
-                $found = true;
-                break;
-            }
-        }
-
-        $remotehost = 	 $_SERVER['REMOTE_HOST']; 
-
-        foreach ($knownhostsuser as $regexp) {
-            if (preg_match($regexp,$remotehost)) {
-                $found = true;
-                break;
-            }
-        }
-
-        return $found;
-    }
-
-
-    private static function isRRZEUser() { 
+    private static function isDevUser() { 
         if (class_exists('\RRZE\Settings\Helper')) {
             // rrze-settings is used
             return \RRZE\Settings\Helper::isRRZEAdmin();
@@ -156,19 +307,14 @@ class Debug {
         }
 
         $knownhostsuser = [
-            '/unrz59\.vpn\.rrze\.uni\-erlangen\.de/i',
-            '/zo95zofo\.vpn\.rrze\.uni\-erlangen\.de/i',
-            '/unrz228\.vpn\.rrze\.uni\-erlangen\.de/i',
-            '/unrz244\.vpn\.rrze\.uni\-erlangen\.de/i',
-            '/we53buko\.vpn\.rrze\.uni\-erlangen\.de/i'
+            '/localhost/i'
         ];
          $knownhostadresses = [
-            '/^131\.188\.73/i',
-            '/^10\.188\.76/i',
-            '/^10\.11\.82/i',
-            '/^10\.11\.216/i',
-             '/^10\.11\.83\.208/i'
-        ];
+            '/^192\.168\./i',
+            '/^127\.0\.0/i',
+            '/^10\.10\./i',
+            '/^10\.11\./i',
+         ];
 
         $found = false;	  
         if (isset($_SERVER['REMOTE_ADDR'])) {
